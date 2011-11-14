5449410032d9004cb4d5bb542393d677c6d3cc9f
G. Grothendieck <ggrothendieck@gmail.com>
2011-11-14 00:00:00 +0000

    version 0.4-4

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,19 @@
 
-.First.lib <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
+
+	# append sqldf library directory to AWKPATH (. is always searched so it
+	# need not be specified)
+	sqldf.home <- normalizePath(file.path(libname, pkgname))
+	AWKPATH <- Sys.getenv("AWKPATH")
+	if (is.null(AWKPATH)) Sys.setenv(AWKPATH=sqldf.home)
+	else {
+		# if awkpath exists but is lower case then references will fail 
+		# so delete it and replace it to be sure
+		Sys.setenv(AWKPATH="")
+		pathsep <- if (.Platform$OS.type == "windows") ";" else ":"
+		Sys.setenv(AWKPATH=paste(AWKPATH, sqldf.home, sep = pathsep))
+	}
+
 	drv <- getOption("sqldf.driver")
 	drv <- if (is.null(drv) || drv == "") {
 		if ("package:RpgSQL" %in% search()) { 
@@ -18,3 +32,5 @@
 	}
 }
 
+.onUnload <- function(libpath) {
+}
